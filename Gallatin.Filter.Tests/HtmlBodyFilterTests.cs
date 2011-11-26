9279fd3320a814fa5f96e34b4557e5c055063b59
Bill O'Neill <william.w.oneill@gmail.com>
2011-11-26 09:08:06 -0700

    Updated unit tests for new filtering capability

--- Gallatin.Filter.Tests/HtmlBodyFilterTests.cs
+++ Gallatin.Filter.Tests/HtmlBodyFilterTests.cs
@@ -14,43 +14,65 @@ namespace Gallatin.Filter.Tests
     [TestFixture]
     class HtmlBodyFilterTests
     {
-        [Test]
-        public void SimpleLoadTest()
+        private byte[] Setup( string htmlBody )
         {
             string xml =
                 "<?xml version='1.0' encoding='utf-8' ?>" +
                 "<ContentFiltering>" +
                 "<BannedWords>" +
-                @"<Regex name='test filter' value='(prox\w{1,4})' weight='50'/>"+
+                @"<Regex name='test filter' value='(prox\w{1,4})' weight='50'/>" +
                 "</BannedWords>" +
                 "</ContentFiltering>";
 
             XDocument document = XDocument.Parse(xml);
 
             Mock<ISettingsFileLoader> mockLoader = new Mock<ISettingsFileLoader>();
-            mockLoader.Setup(m => m.LoadFile(SettingsFileType.HtmlBodyFilter)).Returns(document);   
+            mockLoader.Setup(m => m.LoadFile(SettingsFileType.HtmlBodyFilter)).Returns(document);
 
             Mock<ILogger> mockLogger = new Mock<ILogger>();
-         
+
             Mock<IHttpHeaders> headers = new Mock<IHttpHeaders>();
             headers.SetupGet(m => m["content-type"]).Returns("text/html");
 
             Mock<IHttpResponse> response = new Mock<IHttpResponse>();
             response.SetupGet(m => m.Headers).Returns(headers.Object);
 
-            HtmlBodyFilter filter = new HtmlBodyFilter( mockLoader.Object, mockLogger.Object );
+            HtmlBodyFilter filter = new HtmlBodyFilter(mockLoader.Object, mockLogger.Object);
 
             Func<IHttpResponse, string, byte[], byte[]> outVal;
-            var filterResponse = filter.EvaluateFilter( response.Object, "connectionid", out outVal );
+            var filterResponse = filter.EvaluateFilter(response.Object, "connectionid", out outVal);
 
             Assert.That(filterResponse, Is.Null);
             Assert.That(outVal, Is.Not.Null);
 
-            var htmlBody = Encoding.UTF8.GetBytes( "this string contains the word proxy once and proxy twice so it should get some weight" );
+            var convertedHtmlBody = Encoding.UTF8.GetBytes(htmlBody);
 
-            var filterResponse2 = outVal( response.Object, "connectionid", htmlBody );
+            return outVal(response.Object, "connectionid", convertedHtmlBody);
+            
+        }
 
-            Assert.That( filterResponse2, Is.Not.Null );
+        [Test]
+        public void SimpleLoadTest()
+        {
+            var filterResponse = Setup( "this string contains the word proxy once and proxy twice so it should get some weight" );
+
+            Assert.That( filterResponse, Is.Not.Null );
+        }
+
+        [Test]
+        public void MetaRtaFilterTest()
+        {
+            var filterResponse = Setup("<meta name=\"RATING\" content=\"RTA-5042-1996-1400-1577-RTA\">");
+
+            Assert.That(filterResponse, Is.Not.Null);
+        }
+
+        [Test]
+        public void MetaFilterTest()
+        {
+            var filterResponse = Setup("<META name=\"Rating\" CONTENT=\"mature\">");
+
+            Assert.That(filterResponse, Is.Not.Null);
         }
     }
 }
