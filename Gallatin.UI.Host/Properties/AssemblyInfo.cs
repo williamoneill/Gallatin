ef0ed7826f0a9d684f9779eb10f62e8041e97746
Brandon Johnson <bj16060@gmail.com>
2012-04-11 20:25:46 -0600

    Add Gallatin.UI.Host project to sln

---
+++ Gallatin.UI.Host/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Gallatin.UI.Host")]
+[assembly: AssemblyDescription("Web Admin UI Host")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("Gallatin.UI.Tests")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d4945566-b97c-4e0a-8d9f-26bb3aebd4da")]
